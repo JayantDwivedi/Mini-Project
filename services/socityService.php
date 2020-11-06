99913f30216e3d45a32718d0d25ea8b3af6c9398
JayantDwivedi <jayantdwivedi18@gmail.com>
2020-11-06 23:11:29 +0530

    society db connect

--- services/socityService.php
+++ services/socityService.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,14 +17,16 @@
 
 
 </head>
+
 <body>
 
     <!-- Navigation Bar  -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="navigationBar">
-    
+
         <a class="navbar-brand" href="#">SANSERV</a>
-        
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -40,7 +43,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">About Us</a>
                 </li>
-                
+
                 <li class="nav-item">
                     <a class="nav-link" href="#">Testimonials</a>
                 </li>
@@ -48,7 +51,7 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact Us</a>
                 </li>
-            
+
             </ul>
 
             <form class="form-inline my-2 my-lg-0">
@@ -60,9 +63,9 @@
     <!-- Content Part  -->
     <section class="background-img">
         <div class="row">
-           
+
             <div class="col-lg-2 menu-bar">
-                <div id="navigationBox" >
+                <div id="navigationBox">
                     <h4 id="head">Quick Links</h4>
                     <ul>
                         <li class="py-4 my-2"><a href="houseService.html"> House Sanitization </a></li>
@@ -76,61 +79,68 @@
                     </ul>
                 </div>
             </div>
-           
+
             <div class="col-lg-10 service-tag">
-                
+
                 <h1 id="head1">Services</h1>
-                
+
                 <div class="row">
                     <div class="col-lg-6 form-sec">
                         <div id="formBox">
                             <div>
                                 <h2 class="text-center">SOCIETY SERVICE FORM</h2>
                             </div>
-                    
-                            <div >
+
+                            <div>
                                 <form action="" method="post">
-                                    
+
                                     <div class="form-group">
                                         <label> Name </label>
-                                        <input type="text" name="user" autocomplete="off" class="form-control" placeholder="Enter Name">
+                                        <input type="text" name="user" autocomplete="off" class="form-control"
+                                            placeholder="Enter Name">
                                     </div>
-                    
+
                                     <div class="form-group">
                                         <label> E-mail </label>
-                                        <input type="text" name="email" autocomplete="off" class="form-control" placeholder="Enter Email ID">
+                                        <input type="text" name="email" autocomplete="off" class="form-control"
+                                            placeholder="Enter Email ID">
                                     </div>
-                    
+
                                     <div class="form-group">
                                         <label> Mobile Number </label>
-                                        <input type="text" name="mobile" autocomplete="off" class="form-control" placeholder="Enter Mobile Number">
+                                        <input type="text" name="mobile" autocomplete="off" class="form-control"
+                                            placeholder="Enter Mobile Number">
                                     </div>
 
                                     <div class="form-group">
                                         <label> Address </label>
-                                        <input type="text" name="address" autocomplete="off" class="form-control" placeholder="Enter Address">
+                                        <input type="text" name="address" autocomplete="off" class="form-control"
+                                            placeholder="Enter Address">
                                     </div>
-                                    
+
                                     <div class="form-group">
                                         <label> Appointment Date </label>
-                                        <input type="date" name="date" autocomplete="off" class="form-control" placeholder="Enter Appointment Date">
+                                        <input type="date" name="date" autocomplete="off" class="form-control"
+                                            placeholder="Enter Appointment Date">
                                     </div>
 
                                     <div class="form-group">
                                         <label> Contact Time </label>
-                                        <input type="time" name="time" autocomplete="off" class="form-control" placeholder="Enter Contact Time">
+                                        <input type="time" name="time" autocomplete="off" class="form-control"
+                                            placeholder="Enter Contact Time">
                                     </div>
 
-                                    
 
-    
+
+
                                     <div class="form-group">
-                                        <label>Number of Flats:  </label>
-                                        <input type="number" name="flats" autocomplete="off" class="form-control" placeholder="Enter Number Of Flates">
+                                        <label>Number of Flats: </label>
+                                        <input type="number" name="flats" autocomplete="off" class="form-control"
+                                            placeholder="Enter Number Of Flates">
                                     </div>
 
-        
-                    
+
+
                                     <!-- <label>House Type : </label>
                                     <div class="form-check form-check-inline ">
                                         <input type="checkbox" class="form-check-input px-3" id="exampleCheck1">
@@ -154,11 +164,11 @@
 
                                     <label>Comments </label>
                                     <br>
-                                    <textarea name="comment" id="" cols="74" ></textarea>
-                    
+                                    <textarea name="comment" id="" cols="74"></textarea>
+
                                     <br>
-                                    <button type="submit" class="btn btn-secondary">Submit</button>
-                                    
+                                    <button type="submit" name="b1" class="btn btn-secondary">Submit</button>
+
                                 </form>
                             </div>
                         </div>
@@ -166,13 +176,13 @@
                     <div class="col-lg-5 img-sec">
                         <!-- <h1 class="text-center">Image</h1> -->
                         <img src="../images/img2-carousel.jpg" alt="" class="img-fluid mt-5 pt-5" id="image1">
-                       
+
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    
+
     <!-- Footer Section  -->
 
     <footer class="">
@@ -184,8 +194,8 @@
                 <ul>
                     <li><a href="#">Sanitization</a></li>
                     <li><a href="#">Pesticides</a></li>
-                    <li><a href="#">Disinfection</a></li>    
-                
+                    <li><a href="#">Disinfection</a></li>
+
                 </ul>
             </section>
 
@@ -195,7 +205,7 @@
                 <ul>
                     <li><a href="#">About Us</a></li>
                     <li><a href="#">Contact Us</a></li>
-                    <li><a href="#"> Contribute  </a></li>
+                    <li><a href="#"> Contribute </a></li>
                     <li><a href="#">Privacy Policy</a></li>
                     <li><a href="#">Services</a></li>
                     <!-- <li><a href="#">Disclaimer</a></li> -->
@@ -223,7 +233,7 @@
                 </ul>
             </section>
 
-            
+
         </section>
         <p class="p-3 bg-dark text-white text-center"> © SANSERV | 2020 </p>
 
@@ -231,6 +241,35 @@
     </footer>
 
 
-    
+
 </body>
+
 </html>
+
+<?php
+include '../connection.php';
+
+if (isset($_POST['b1'])) {
+
+    $name = $_POST['user'];
+    $email = $_POST['email'];
+    $mob = $_POST['mobile'];
+    $address = $_POST['address'];
+    $ts = $_POST['time'];
+    $ds = $_POST['date'];
+    $flats = $_POST['flats'];
+    $comments = $_POST['comment'];
+
+    if ($name != "" && $email != "" && $mob != "" && $address != "" && $ts != "" && $ds != "" && $comments != "") {
+        $query = "insert into societyservice (name,email,mobile,address,time,date,flats,comment) values('$name','$email','$mob','$address','$ts','$ds','$flats','$comments')";
+        $data = mysqli_query($connection, $query) or die(mysqli_error($connection));
+        if ($data) {
+            echo "Record Inserted Succesfully";
+        } else {
+            echo "Data not Inserted";
+        }
+    } else {
+        echo "<p style='color:red'> All Fields are required</p>";
+    }
+}
+?>
