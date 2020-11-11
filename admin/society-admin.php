af2041f8a91762d5f07a73b534dcfaea43a0221c
JayantDwivedi <jayantdwivedi18@gmail.com>
2020-11-11 17:28:25 +0530

    society updated

--- admin/society-admin.php
+++ admin/society-admin.php
@@ -114,7 +114,8 @@
                   <td>" . $result['date'] . "</td>
                   <td>" . $result['flats'] . "</td>
                   <td>" . $result['comment'] . "</td>
-                  <td><a href='delete/delete-society.php?rn=$result[mobile]' onclick = 'return DeleteRecord()'><i style='color:red;font-size:24px;' class='fa fa-trash'></i></a></h4></td>
+                  <td><a href='delete/delete-society.php?rn=$result[mobile]' onclick = 'return DeleteRecord()'><i style='color:red;font-size:24px;' class='fa fa-trash'></i></a></h4>
+                  <a href='update/update-society.php?rn=$result[name]&nm=$result[mobile]&cl=$result[email]&ad=$result[address]&tm=$result[time]&dt=$result[date]&cm=$result[comment]&fl=$result[flats]'><i style='color:blue;font-size:24px;margin-left:25px;' class='fa fa-pencil'></i></a></td>
                 </tr>
                 ";
                     }
